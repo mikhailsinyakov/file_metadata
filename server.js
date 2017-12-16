28c7ba4ed695f79dc3a83b216a3c7e10bd79733a
Glitch (descriptive-brian) <none>
2017-12-16 09:09:33 +0000

    💝💕 Checkpoint public/index.html:586560 server.js:586560

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ const port = process.env.PORT;
 
 app.use(express.static('public'));
 
+app.post("/get-file-size", (req, res) => {
+  
+  console.log(req.get("Content-Type"));
+  const obj = {
+    size: ""
+  }
+  res.json(obj);
+});
 
 app.listen(port, () => {
   console.log('Your app is listening on port ' + port);
