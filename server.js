476f4f5f1db8998e99a76cec84d78ae59b6f15f0
Glitch (descriptive-brian) <none>
2017-12-16 11:01:57 +0000

    🦀🍐 Checkpoint server.js:586560 package.json:586560 public/index.html:586560

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const app = express();
 const multer = require("multer");
+const bodyParser = require("body-parser");
 
 const port = process.env.PORT;
-const upload = multer(/*{dest: "uploads/"}*/);
+const storage = multer.diskStorage({
+  destination: (request, file, callback) => {
+    callback(null, "./uploads");
+  },
+  filename: (request, file, callback) => {
+    console.log(file);
+    callback(null, file.originalname)
+  }
+})
+const upload = multer({dest: "./uploads/"});
 
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
-app.post("/get-file-size", upload.array(), (req, res) => {
+app.post("/get-file-size", upload.single('file'), (req, res) => {
   
   console.log(req.body);
   const obj = {
